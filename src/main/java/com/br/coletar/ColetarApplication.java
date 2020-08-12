c458223847a6cfc4b71a7ea0e1025e2a961965eb
bruno <brunoliracz@gmail.com>
2020-08-12 10:56:52 -0300

    criando o projeto

---
+++ src/main/java/com/br/coletar/ColetarApplication.java
@@ -0,0 +1,13 @@
+package com.br.coletar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColetarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColetarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/coletar/ColetarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.coletar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColetarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
